2df4d4daaf2033f117db783f2bf46076c2eaa55d
Weihe Wang <weihewang2012@gmail.com>
2018-07-25 14:08:47 -0700

    Update xml_parser.py

--- tool_scripts/xml_parser.py
+++ tool_scripts/xml_parser.py
@@ -6,44 +6,58 @@ import itertools
 class XML_processor:
     def __init__(self, folder_path=None):
         self.folder = folder_path
-        self.data = defaultdict(list)
-    
+        self.data = []
+
     def result_generation_helper(self, xml):
         root = ET.parse(xml).getroot()
-        
+
+        # Initialize a dict of lists for storing test results
+        # test results are initially collected as dict[list[dict]]
         test_results = defaultdict(list)
+
         for collection in root.iter('TestCollection'):
+            # Typically the file only contains one collection
             for test in collection.findall('Test'):
+                # looping through all test result collections
                 # each test has to have a name
                 testarea = test.find('Name').text.rstrip()
                 for ds_collection in test.findall('DataSetCollection'):
+                    # looping through each test case in test collection
                     # may not exist
                     collection_name = ds_collection.find('Name')
 
+                    # store result in each test case in a dict
                     temp = defaultdict()
 
                     for d in ds_collection.iter('DI'):
                         n, v = d.find('N'), d.find('V')
                         temp[n.text] = v.text
+
+                    # attempt to use collection name
+                    # if not successful, it is an input section
                     if collection_name is not None:
                         test_results[testarea +' ' + collection_name.text].append(temp)
                     else:
                         test_results[testarea + ' Inputs'].append(temp)
-        print(test_results['LTE_SpectrumEmissionMask SEM Detail'])
 
+        # Given test_results in form dict[list[dict]]: testcase -> list of occurrences -> KPI
+        # Transform to dict[dict[list]]: testcase -> KPI -> list of results
+        final_res = defaultdict(list)
         for testcase in test_results:
+            # KPI -> list of results
             d = defaultdict(list)
             for it in test_results[testcase]:
                 for kpi in it:
                     d[kpi].append(it[kpi])
-            self.data[testcase].append(d)
-    
+            final_res[testcase].append(d)
+        self.data.append(final_res)
+
     def collect_data(self):
         if self.folder is None:
             self.folder = input()
         for xml_file in Path(self.folder).glob('*.xml'):
             self.result_generation_helper(xml_file)
-    
+            
                 
 class Get_setup_info(object):
     
