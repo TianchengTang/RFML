130467304d53fe2c7bdc3444795f213f7cb8b9d2
Weihe Wang <weihewang2012@gmail.com>
2018-08-20 18:02:54 -0700

    Update dynamic_class.py
    Add documentation and more formality.

--- tool_scripts/dynamic_class.py
+++ tool_scripts/dynamic_class.py
@@ -1,3 +1,7 @@
+"""
+Dynamic class reference:
+    https://www.python-course.eu/python3_classes_and_type.php
+"""
 from mongoengine import *
 
 # Read test case names
@@ -6,26 +10,51 @@ with open('test_case_names.txt', 'rt') as f:
     for item in f.readlines():
         tests.append(item.rstrip())
 
-# generate classes for all tests
-test_classes = {}   # test case to class mapping
+def generate_document_classes(tests):
+    """
+        generate classes for all tests.
 
-for test in tests:
+        tests: List[str]
+            test case names
+        return: dict
+            dictionary of class name -> class mapping
+
+    """
+    test_classes = {}   # test case to class mapping
+
+    # define common fields for all document classes
     values = {
-    'uuid': StringField(default='12345'),
-    'testcasename': StringField(default='test', required=True),
-    'data': DictField(),
-}
+        'uuid': StringField(default='12345'),
+        'testcasename': StringField(default='test', required=True),
+        'data': DictField(),
+    }
 
-    test_classes[test] = type(
-        test,
-        (Document, ),
-        values,
-    )
+    for test in tests:
+        test_classes[test] = type(
+            test,
+            (Document, ),
+            values,
+        )
+    return test_classes
+
+def write_to_db(processor, test_classes):
+    """
+        Save all data to MongoDB.
+
+        processor: XML_processor
+            XML Processor containing data from XML files
+        test_classes: dict
+            dictionary of class name -> class mapping
+        return: None
+
+    """
+    for xml_doc in processor.data:
+        for testcase in xml_doc:
+            doc_class = test_classes[testcase]
+            new_doc = doc_class(testcasename=testcase, data=xml_doc[testcase])
+            new_doc.save()
 
 
-## Save all data to MongoDB
-for doc in processor.data:
-    for testcase in doc:
-        doc_class = test_classes[testcase]
-        new_doc = doc_class(testcasename=testcase, data=doc[testcase])
-        new_doc.save()
+if __name__ == '__main__':
+    test_classes = generate_document_classes()
+    write_to_db(processor, test_classes)
