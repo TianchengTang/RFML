2298f277d5cf557719169cdcf8cd002768543579
Weihe Wang <weihewang2012@gmail.com>
2018-08-28 17:12:50 -0700

    Update xml_parser.py

--- tool_scripts/xml_parser.py
+++ tool_scripts/xml_parser.py
@@ -1,61 +1,161 @@
 import xml.etree.ElementTree as ET
 import pandas as pd
-from collections import defaultdict
+from collections import defaultdict, OrderedDict
 import itertools
 import pymongo
 
-class Xml_parser:
-    """
-    Main parser for collecting information from past test result XML files.
-    """
-    def __init__(self, input_fp='', output_fp=None):
-        """
-        input_fp: XML path
-        output_fp: Excel/CSV path
-        """
-        self.root = ET.parse(input_fp).getroot()
-        if not output_fp:
-            output_fp = input()
-        self.output_fp = output_fp
-        self.tables = None
+class XML_parser:
+    def __init__(self, folder_path=None):
+        self.folder = folder_path
+        self.data = []
+        self.testcases = set()
 
-    def result_generation_helper(self):
+    def precheck(self, xml):
         """
-        Core data processing function for XML. Subject to changes as input format and requirements change.
-        :return: None
+        rtype: str
+            returns "success" if all operations successful til call setup;
+            else, returns failure reason.
         """
-        test_results = defaultdict(list)    # raw data collection
-        for ds_collection in self.root.iter('DataSetCollection'):
-            # Each test case run is organized under data set collection
-            iterators = [ds_collection.iter('Name'), ds_collection.iter('Outputs'), ds_collection.iter('Inputs')]
-            for name, output, inputs in zip(*iterators):    # for each testcase name, output collection and input collection...
-                temp = defaultdict()
-                for d in itertools.chain(inputs.iter('DI'), output.iter('DI')): # combine two iterators and loop over
-                    for n, v in zip(d.iter('N'), d.iter('V')):                  # collect key-value pairs
-                        temp[n.text] = v.text
-                test_results[name.text].append(temp)                      # dictionary in form  of testcase->list of results(dict)
-        final_res = defaultdict(dict)                                     # reshape data to form of testcase->dictionary of lists
+        root = ET.parse(xml).getroot()
+        precheck_result = OrderedDict()
+        for testname in root.iter("Test"):
+            ## using ordered dict allows adding operations in sequence.
+            if testname.attrib.get('I') == "3095":
+                for child in testname.iter("PassFail"):
+                    precheck_result['phonecomm'] = [i.text for i in child.iter("V")][0]
+            if testname.attrib.get('I') == "10105":
+                for child in testname.iter("PassFail"):
+                    precheck_result['initbse'] = [i.text for i in child.iter("V")][0]
+            if testname.attrib.get('I') == "10110":
+                for child in testname.iter("PassFail"):
+                    precheck_result['registration'] = [i.text for i in child.iter("V")][0]
+            if testname.attrib.get('I') == "10115":
+                for child in testname.iter("PassFail"):
+                    precheck_result['callup'] = [i.text for i in child.iter("V")][0]
+        for operation in precheck_result:
+            if precheck_result[operation] != 'PASS':
+                return operation
+        return 'success'
+    
+        
+    def result_generation_helper(self, xml):
+        root = ET.parse(xml).getroot()
+
+        # Initialize a dict of lists for storing test results
+        # test results are initially collected as dict[list[dict]]
+        test_results = defaultdict(list)
+
+        for collection in root.iter('TestCollection'):
+            # Typically the file only contains one collection
+            for test in collection.findall('Test'):
+                # looping through all test result collections
+                # each test has to have a name
+                testarea = test.find('Name').text.rstrip()
+                for ds_collection in test.findall('DataSetCollection'):
+                    # looping through each test case in test collection
+                    # may not exist
+                    collection_name = ds_collection.find('Name')
+                    
+                    # one collection may have multiple data sets
+                    for dataset in ds_collection.findall('DataSet'):
+
+                        # store result in each test case in a dict
+                        temp = defaultdict()
+                        
+                        # get all input key - value pairs, there might be numerous
+                        inputs = dataset.find('Inputs')
+                        for d in inputs.iter('DI'):
+                            n, v = d.find('N'), d.find('V')
+                            temp[n.text] = v.text
+                            
+                        # There might be multiple results in each output
+                        # For each result, get name and use name to construct limit name
+                        outputs = dataset.find('Outputs')
+                        for result in outputs.findall('Result'):
+                            kpi_name = result.find('DI').find('N')
+                            kpi = result.find('DI').find('V')
+                            temp[kpi_name.text] = kpi.text
+                            
+                            # limit related search
+                            limit = result.find('Limits')
+                            
+                            if limit is None:
+                                # limit does not exist
+                                continue
+                                
+                            max_limit = limit.find('Max')
+                            if max_limit is not None:
+                                temp[kpi_name.text+'MAX_limit'] = max_limit.text
+                            
+                            min_limit = limit.find('Min')
+                            if min_limit is not None:
+                                temp[kpi_name.text+'MIN_limit'] = min_limit.text
+                                
+                            kpi_result = limit.find('PassFail').find('V')
+                            if kpi_result is not None:
+                                temp[kpi_name.text+'Result'] = kpi_result.text
+
+                        # attempt to use collection name
+                        # if not successful, it is an input section, use 'Input' instead
+                        if collection_name is not None:
+                            test_results['_'.join((testarea + collection_name.text).split()).lower()].append(temp)
+                        else:
+                            test_results['_'.join((testarea + 'Inputs').split()).lower()].append(temp)
+
+        # Given test_results in form dict[list[dict]]: testcase -> list of occurrences -> KPI
+        # Transform to dict[dict[list]]: testcase -> KPI -> list of results
+        final_res = defaultdict()
         for testcase in test_results:
+            self.testcases.add(testcase)
+            # Transform to KPI -> list of results
             d = defaultdict(list)
             for it in test_results[testcase]:
                 for kpi in it:
                     d[kpi].append(it[kpi])
             final_res[testcase] = d
-        self.tables = final_res
+        self.data.append(final_res)
 
-    def generate_excel(self):
-        if not self.tables:
-            # if table does not exist already, generate tables
-            self.result_generation_helper()
-        writer = pd.ExcelWriter(self.output_fp)
-        for test in self.tables:
-            test_name = test
-            if len(test_name) > 31:
-                test_name = test_name[:30]
-            try:
-                pd.DataFrame.from_dict(self.tables[test], orient='index').T.to_excel(writer, test_name)
-            except:
-                continue
+    def collect_data(self):
+        if self.folder is None:
+            self.folder = input()
+        for xml_file in Path(self.folder).glob('*.xml'):
+            check_result = self.precheck(xml_file)
+            if check_result == 'success':
+                self.result_generation_helper(xml_file)
+            else:
+                print(check_result)
+              
+            
+class mongodb_filler:
+    def __init__(self, xml_parser):
+        """
+        xml_parser: XML_parser
+            data source
+        """
+        self.xml_parser = xml_parser
+        
+    def generate_classes(self):
+        doc_classes = {}
+        for test in self.xml_parser.testcases:
+            values = {
+                'uuid': StringField(default='12345'),
+                'testcasename': StringField(default='test', required=True),
+                'data': DictField(),
+            }
+            doc_classes[test] = type(test, 
+                                     (mongoengine.Document, ), 
+                                     values,
+                                    )                                 
+        self.doc_classes = doc_classes
+        
+    def fill_database(self):
+        self.generate_classes()
+        for doc in self.xml_parser.data:
+            for testcase in doc:
+                doc_class = self.doc_classes[testcase]
+                new_doc = doc_class(testcasename=testcase, data=doc[testcase])
+                new_doc.save()
+                
                 
 class Get_setup_info(object):
     
